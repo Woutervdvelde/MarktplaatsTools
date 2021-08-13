d3daad191235d2a1c814ca679124af750fd4fd84
Wouter <woutervandervelde2000@hotmail.com>
2021-08-13 17:04:59 +0200

    added contextmenu for searching selected text directly in marktplaats

--- background.js
+++ background.js
@@ -7,4 +7,19 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ settings });
   console.log("set settings:")
   console.log(settings);
+
+  chrome.contextMenus.create({id: "MTSearch", contexts: ['selection'], title: "Zoek op Marktplaats"}, ()=>{} );
+});
+
+const searchMarktplaats = (query) => {
+  const baseUrl = "https://www.marktplaats.nl/q/"
+  chrome.tabs.create({ url: baseUrl + query });
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "MTSearch":
+      searchMarktplaats(info.selectionText);
+      break;
+  }
 });
